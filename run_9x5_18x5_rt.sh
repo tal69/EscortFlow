b909eb80bb33887ddd16340db5eac14502179053
talraviv <talraviv69@gmail.com>
2024-07-05 15:44:16 +0300

    July 5, 2024. Starting to warpup

---
+++ run_9x5_18x5_rt.sh
@@ -0,0 +1,13 @@
+python EscortFlowSim_v2.py  -x 9 -y 5 -O 4 0  -e 4 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.2 -M 10 -q spt -H
+python EscortFlowSim_v2.py  -x 9 -y 5 -O 4 0  -e 4 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.4 -M 10 -q spt 
+python EscortFlowSim_v2.py  -x 9 -y 5 -O 4 0  -e 8 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.2 -M 10 -q spt 
+python EscortFlowSim_v2.py  -x 9 -y 5 -O 4 0  -e 8 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.4 -M 10 -q spt 
+python EscortFlowSim_v2.py  -x 9 -y 5 -O 4 0  -e 12 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.2 -M 10 -q spt 
+python EscortFlowSim_v2.py  -x 9 -y 5 -O 4 0  -e 12 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.4 -M 10 -q spt 
+
+python EscortFlowSim_v2.py  -x 18 -y 5 -O 4 0 13 0  -e 8 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.4 -M 15 -q spt 
+python EscortFlowSim_v2.py  -x 18 -y 5 -O 4 0 13 0  -e 8 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.8 -M 15 -q spt 
+python EscortFlowSim_v2.py  -x 18 -y 5 -O 4 0 13 0  -e 16 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.4 -M 15 -q spt 
+python EscortFlowSim_v2.py  -x 18 -y 5 -O 4 0 13 0  -e 16 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.8 -M 15 -q spt 
+python EscortFlowSim_v2.py  -x 18 -y 5 -O 4 0 13 0  -e 24 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.4 -M 15 -q spt 
+python EscortFlowSim_v2.py  -x 18 -y 5 -O 4 0 13 0  -e 24 -S 2000 -T 4 -I 1 -E 1 -t 1 -R 0.8 -M 15 -q spt 
