b3ef855eb94716bd619ce5cba19188e80aeb3e1f
talraviv <talraviv69@gmail.com>
2026-03-14 20:36:53 +0200

    Shell files

---
+++ run_9x5_18x5_27x5_rt.sh
@@ -0,0 +1,15 @@
+python EscortFlowSim_v5.py  -x 9 -y 5 -O 4 0  -e 4 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 0.2 -M 6 -q spt -H
+python EscortFlowSim_v5.py  -x 9 -y 5 -O 4 0  -e 4 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 0.4 -M 6 -q spt 
+python EscortFlowSim_v5.py  -x 9 -y 5 -O 4 0  -e 8 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 0.2 -M 6 -q spt 
+python EscortFlowSim_v5.py  -x 9 -y 5 -O 4 0  -e 8 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 0.4 -M 6 -q spt 
+
+python EscortFlowSim_v5.py  -x 18 -y 5 -O 4 0 13 0  -e 8 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 0.4 -M 6 -q spt 
+python EscortFlowSim_v5.py  -x 18 -y 5 -O 4 0 13 0  -e 8 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 0.8 -M 6 -q spt 
+python EscortFlowSim_v5.py  -x 18 -y 5 -O 4 0 13 0  -e 16 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 0.4 -M 6 -q spt 
+python EscortFlowSim_v5.py  -x 18 -y 5 -O 4 0 13 0  -e 16 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 0.8 -M 6 -q spt 
+
+python EscortFlowSim_v5.py  -x 27 -y 5 -O 4 0 13 0 22 0 -e 12 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 0.6 -M 6 -q spt 
+python EscortFlowSim_v5.py  -x 27 -y 5 -O 4 0 13 0 22 0 -e 12 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 1.2 -M 6 -q spt 
+python EscortFlowSim_v5.py  -x 27 -y 5 -O 4 0 13 0 22 0 -e 24 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 0.8 -M 6 -q spt 
+python EscortFlowSim_v5.py  -x 27 -y 5 -O 4 0 13 0 22 0 -e 24 -S 1600 -T 5 -I 1 -E 1 -t 1 -R 1.2 -M 6 -q spt 
+
