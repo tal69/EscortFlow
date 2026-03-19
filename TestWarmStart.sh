e435c2dde9c1fad64cc841e6540e543f8a5a0830
talraviv <talraviv69@gmail.com>
2026-03-19 08:42:43 +0200

    Warmup

---
+++ TestWarmStart.sh
@@ -0,0 +1,7 @@
+python EscortFlowSim_v7.py -L -x 5 -y 9 -O 4 0 -R 0.4 -S 500 --hybrid  --hybrid_ratio 1 -f Warmstart.csv -I 5
+python EscortFlowSim_v7.py -L -x 5 -y 9 -O 4 0 -R 0.4 -S 500 --hybrid  --hybrid_ratio 1 -f Warmstart.csv -I 5  --warmstart
+python EscortFlowSim_v7.py -L -x 5 -y 9 -O 4 0 -R 0.4 -S 500 --hybrid  --hybrid_ratio 1 -f Warmstart.csv --warmstart
+
+python EscortFlowSim_v7.py -L -x 5 -y 9 -O 4 0 -R 0.1 -S 500 --hybrid  --hybrid_ratio 1 -f Warmstart.csv -I 5
+python EscortFlowSim_v7.py -L -x 5 -y 9 -O 4 0 -R 0.1 -S 500 --hybrid  --hybrid_ratio 1 -f Warmstart.csv -I 5  --warmstart
+python EscortFlowSim_v7.py -L -x 5 -y 9 -O 4 0 -R 0.1 -S 500 --hybrid  --hybrid_ratio 1 -f Warmstart.csv --warmstart
