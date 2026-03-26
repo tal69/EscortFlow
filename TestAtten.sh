20f6a35728dc6aafa0feee4ef1f6a13792fae9a4
talraviv <talraviv69@gmail.com>
2026-03-26 19:21:48 +0200

    TestHybridRatio.sh

--- TestAtten.sh
+++ TestAtten.sh
@@ -1,13 +1,13 @@
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 4 -M 7 -S 1000 --hybrid -M 5 -f AttentionTest.csv
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 4 -M 7 -S 1000 --hybrid -M 6  -f AttentionTest.csv
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 4 -M 7 -S 1000 --hybrid -M 7  -f AttentionTest.csv
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 4 -M 7 -S 1000 --hybrid -M 8  -f AttentionTest.csv
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 4 -M 7 -S 1000 --hybrid -M 9  -f AttentionTest.csv
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 4 -M 7 -S 1000 --hybrid  -f AttentionTest.csv
+python EscortFlowSim_v8.py -x  9 -y 5 -O 4 0 -R 0.4 -e 4 -M 5 -S 1000 --hybrid -M 5 -f AttentionTest.csv
+python EscortFlowSim_v8.py -x  9 -y 5 -O 4 0 -R 0.4 -e 4 -M 6 -S 1000 --hybrid -M 6  -f AttentionTest.csv
+python EscortFlowSim_v8.py -x  9 -y 5 -O 4 0 -R 0.4 -e 4 -M 7 -S 1000 --hybrid -M 7  -f AttentionTest.csv
+python EscortFlowSim_v8.py -x  9 -y 5 -O 4 0 -R 0.4 -e 4 -M 8 -S 1000 --hybrid -M 8  -f AttentionTest.csv
+python EscortFlowSim_v8.py -x  9 -y 5 -O 4 0 -R 0.4 -e 4 -M 9 -S 1000 --hybrid -M 9  -f AttentionTest.csv
+python EscortFlowSim_v8.py -x  9 -y 5 -O 4 0 -R 0.4 -e 4 -S 1000 --hybrid  -f AttentionTest.csv
 
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 8 -M 7 -S 1000 --hybrid -M 5 -f AttentionTest.csv
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 8 -M 7 -S 1000 --hybrid -M 6 -f AttentionTest.csv
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 8 -M 7 -S 1000 --hybrid -M 7  -f AttentionTest.csv
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 8 -M 7 -S 1000 --hybrid -M 8  -f AttentionTest.csv
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 8 -M 7 -S 1000 --hybrid -M 9  -f AttentionTest.csv
-python EscortFlowSim_v8.py -x 5 -y 9 -O 4 0 -R 0.4 -e 8 -M 7 -S 1000 --hybrid  -f AttentionTest.csv
+python EscortFlowSim_v8.py -x 13 -y 7 -O 6 0 -R 0.4 -e 8 -M 5 -S 1000 --hybrid -M 5 -f AttentionTest.csv
+python EscortFlowSim_v8.py -x 13 -y 7 -O 6 0 -R 0.4 -e 8 -M 6 -S 1000 --hybrid -M 6 -f AttentionTest.csv
+python EscortFlowSim_v8.py -x 13 -y 7 -O 6 0 -R 0.4 -e 8 -M 7 -S 1000 --hybrid -M 7  -f AttentionTest.csv
+python EscortFlowSim_v8.py -x 13 -y 7 -O 6 0 -R 0.4 -e 8 -M 8 -S 1000 --hybrid -M 8  -f AttentionTest.csv
+python EscortFlowSim_v8.py -x 13 -y 7 -O 6 0 -R 0.4 -e 8 -M 9 -S 1000 --hybrid -M 9  -f AttentionTest.csv
+python EscortFlowSim_v8.py -x 13 -y 7 -O 6 0 -R 0.4 -e 8      -S 1000 --hybrid  -f AttentionTest.csv
