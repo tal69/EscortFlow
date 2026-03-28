86f6e2e24625493322af982de428a2c58f00acbb
talraviv <talraviv69@gmail.com>
2026-03-28 09:49:35 +0300

    TestHybridRatio.sh

--- Table1b.sh
+++ Table1b.sh
@@ -1,7 +1,6 @@
 python EscortFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:20 -l 4  -r 1-100 --gurobi -f table2_escortflow.csv  --time_limit=300
 python EscortFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:20 -l 4  -r 1-100 --gurobi -f table2_escortflow.csv --time_limit=300
-python EscortFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:20 -l 4  -r 1-100 --gurobi -f table2_escortflow.csv --time_limit=300
-python EscortFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:20 -l 4  --bnc -r 1-100 --gurobi -f table2_escortflow_bnc.csv   --time_limit=300
+python EscortFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:20 -l 4  -r 1-100 --gurobi -f table2_escortflow.csv --time_limit=3python EscortFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:20 -l 4  --bnc -r 1-100 --gurobi -f table2_escortflow_bnc.csv   --time_limit=300
 python EscortFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:20 -l 4 --bnc  -r 1-100 --gurobi -f table2_escortflow_bnc.csv --time_limit=300
 python EscortFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:20 -l 4 --bnc -r 1-100 --gurobi -f table2_escortflow.csv --time_limit=300
 python LoadFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:20 -l 4 -r 1-100 --gurobi -f table2_loadflow.csv --time_limit=300
---
+++ TestIntegrated.sh
@@ -0,0 +1,11 @@
+python EscortFlowSim_v8.py -x 18 -y 5 -O 4 0 11 0 -R 0.6 -e 4 -M 7 -S 2000 --hybrid --hybrid_ratio 1 -f Modular_vs_integrated.csv
+python EscortFlowSim_v8.py -x 9 -y 5 -O 4 0 -R 0.3 -e 8 -M 7 -S 2000 --hybrid --hybrid_ratio 1 -f Modular_vs_integrated.csv
+
+python EscortFlowSim_v8.py -x 26 -y 7 -O 6 0 19 0 -R 0.6 -e 16 -M 7 -S 2000 --hybrid --hybrid_ratio 1 -f Modular_vs_integrated.csv
+python EscortFlowSim_v8.py -x 13 -y 7 -O 6 0 -R 0.3 -e 8 -M 7 -S 2000 --hybrid --hybrid_ratio 1 -f Modular_vs_integrated.csv
+
+python EscortFlowSim_v8.py -x 18 -y 5 -O 4 0 11 0 -R 0.6 -e 4 -M 7 -S 2000 --hybrid --hybrid_ratio 1 -f Modular_vs_integrated.csv -t 10
+python EscortFlowSim_v8.py -x 9 -y 5 -O 4 0 -R 0.3 -e 8 -M 7 -S 2000 --hybrid --hybrid_ratio 1 -f Modular_vs_integrated.csv -t 10
+
+python EscortFlowSim_v8.py -x 26 -y 7 -O 6 0 19 0 -R 0.6 -e 16 -M 7 -S 2000 --hybrid --hybrid_ratio 1 -f Modular_vs_integrated.csv -t 10
+python EscortFlowSim_v8.py -x 13 -y 7 -O 6 0 -R 0.3 -e 8 -M 7 -S 2000 --hybrid --hybrid_ratio 1 -f Modular_vs_integrated.csv -t 10
