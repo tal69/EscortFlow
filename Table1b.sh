deb1fbd576d4f1264591f9c055a58128e279bb20
talraviv <talraviv69@gmail.com>
2026-03-29 12:19:50 +0300

    TestHybridRatio.sh

--- Table1b.sh
+++ Table1b.sh
@@ -1,8 +1,11 @@
-python EscortFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:20 -l 4  -r 1-100 --gurobi -f table2_escortflow.csv  --time_limit=300
-python EscortFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:20 -l 4  -r 1-100 --gurobi -f table2_escortflow.csv --time_limit=300
-python EscortFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:20 -l 4  -r 1-100 --gurobi -f table2_escortflow.csv --time_limit=3python EscortFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:20 -l 4  --bnc -r 1-100 --gurobi -f table2_escortflow_bnc.csv   --time_limit=300
-python EscortFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:20 -l 4 --bnc  -r 1-100 --gurobi -f table2_escortflow_bnc.csv --time_limit=300
-python EscortFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:20 -l 4 --bnc -r 1-100 --gurobi -f table2_escortflow.csv --time_limit=300
-python LoadFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:20 -l 4 -r 1-100 --gurobi -f table2_loadflow.csv --time_limit=300
-python LoadFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:20 -l 4  -r 1-100 --gurobi -f table2_loadflow.csv --time_limit=300
-python LoadFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:20 -l 4  -r 1-100 --gurobi -f table2_loadflow.csv --time_limit=300
+python EscortFlowStatic.py -x 9 -y 5 -O 4 0  -e 8:4:16 -l 4 --bnc  -r 1-100 --gurobi -f table1b_escortflow_lp.csv --lp
+python EscortFlowStatic.py -x 13 -y 7 -O 6 0   -e 8:4:16 -l 4 --bnc -r 1-100 --gurobi -f table1b_escortflow_lp.csv --lp
+python EscortFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow_lp.csv  --lp
+python EscortFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow_lp.csv --lp
+python EscortFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow_lp.csv --lp
+
+python LoadFlowStatic.py -x 9 -y 5 -O 4 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f table1b_loadflow.csv --lp
+python LoadFlowStatic.py -x 13 -y 7 -O 6 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f table1b_loadflow.csv --lp
+python LoadFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f table1b_loadflow.csv --lp
+python LoadFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflow.csv --lp
+python LoadFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflow.csv --lp
