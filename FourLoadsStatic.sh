21d05a34caadc8298b86a2d11c12721e37ce8914
talraviv <talraviv69@gmail.com>
2026-05-08 01:43:35 +0300

    refactor: remove lp flag and update output filenames in static execution scripts

--- FourLoadsStatic.sh
+++ FourLoadsStatic.sh
@@ -1,14 +1,14 @@
-python EscortFlowStatic.py -x 9 -y 5 -O 4 0  -e 8:4:16 -l 4   -r 1-100 --gurobi -f table1b_escortflow_lp.csv --lp
-python EscortFlowStatic.py -x 13 -y 7 -O 6 0   -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow_lp.csv --lp
-python EscortFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow_lp.csv  --lp
-python EscortFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow_lp.csv --lp
-python EscortFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow_lp.csv --lp
+python EscortFlowStatic.py -x 9 -y 5 -O 4 0  -e 8:4:16 -l 4   -r 1-100 --gurobi -f table1b_escortflow.csv
+python EscortFlowStatic.py -x 13 -y 7 -O 6 0   -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow.csv
+python EscortFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow.csv 
+python EscortFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow.csv
+python EscortFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflow§.csv
 
-python LoadFlowStatic.py -x 9 -y 5 -O 4 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f table1b_loadflow.csv --lp
-python LoadFlowStatic.py -x 13 -y 7 -O 6 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f table1b_loadflow.csv --lp
-python LoadFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f table1b_loadflow.csv --lp
-python LoadFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflow.csv --lp
-python LoadFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflow.csv --lp
+python LoadFlowStatic.py -x 9 -y 5 -O 4 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f table1b_loadflow.csv
+python LoadFlowStatic.py -x 13 -y 7 -O 6 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f table1b_loadflow.csv
+python LoadFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f table1b_loadflow.csv
+python LoadFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflow.csv
+python LoadFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflow.csv
 
 python EscortFlowStatic.py -x 9 -y 5 -O 4 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflowlp.csv  --lp
 python EscortFlowStatic.py -x 13 -y 7 -O 6 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflowlp.csv  --lp
