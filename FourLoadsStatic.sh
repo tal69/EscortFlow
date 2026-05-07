cd5641d96e8edebe5ee9a2ab99763130f0ba538c
talraviv <talraviv69@gmail.com>
2026-05-08 01:39:08 +0300

    feat: add execution scripts for EscortFlow and updated LoadFlow scenarios to FourLoadsStatic.sh

--- FourLoadsStatic.sh
+++ FourLoadsStatic.sh
@@ -9,3 +9,15 @@ python LoadFlowStatic.py -x 13 -y 7 -O 6 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f t
 python LoadFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:16 -l 4 -r 1-100 --gurobi -f table1b_loadflow.csv --lp
 python LoadFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflow.csv --lp
 python LoadFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflow.csv --lp
+
+python EscortFlowStatic.py -x 9 -y 5 -O 4 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflowlp.csv  --lp
+python EscortFlowStatic.py -x 13 -y 7 -O 6 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflowlp.csv  --lp
+python EscortFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflowlp.csv  --lp
+python EscortFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflowlp.csv --lp
+python EscortFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_escortflowlp.csv --lp
+
+python LoadFlowStatic.py -x 9 -y 5 -O 4 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflowlp.csv  --lp
+python LoadFlowStatic.py -x 13 -y 7 -O 6 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflowlp.csv  --lp
+python LoadFlowStatic.py -x 10 -y 10 -O 0 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f tabel1b_loadflowlp.csv  --lp
+python LoadFlowStatic.py -x 16 -y 10 -O 4 0 11 0  -e 8:4:16 -l 4  -r 1-100 --gurobi -f tabel1b_loadflowlp.csv  --lp
+python LoadFlowStatic.py -x 27 -y 10 -O 4 0 13 0 22 0 -e 8:4:16 -l 4  -r 1-100 --gurobi -f table1b_loadflowlp.csv --lp
